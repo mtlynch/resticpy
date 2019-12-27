5b1fe698c61386109afb7c017b361dca81e4227c
jstzwj <1103870790@qq.com>
2019-12-27 21:15:05 +0800

    upload to pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import setuptools
 
 setuptools.setup(name='pyrestic',
-      version='0.1a1.dev001',
+      version='0.1a1.dev002',
       description='Restic backup Python wrapper',
       author='JunWang',
       author_email='jstzwj@aliyun.com',
