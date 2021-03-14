f0168c6214a9da8ac3c228ff1245a9efcb6a47b3
Michael Lynch <git@mtlynch.io>
2021-03-14 16:11:17 -0400

    Add support for --exclude flag of backup command

--- restic/internal/backup.py
+++ restic/internal/backup.py
@@ -1,7 +1,11 @@
 from restic.internal import command_executor
 
 
-def run(restic_base_command, paths):
+def run(restic_base_command, paths, exclude_patterns=[]):
     cmd = restic_base_command + ['backup'] + paths
+
+    if exclude_patterns:
+        for exclude_pattern in exclude_patterns:
+            cmd.extend(['--exclude', exclude_pattern])
 
     return command_executor.execute(cmd)
