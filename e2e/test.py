4a041efd85d0ac5cd26db09025cf41579d0167b7
Michael Lynch <git@mtlynch.io>
2021-03-14 15:29:19 -0400

    Add support for backup command

--- e2e/test.py
+++ e2e/test.py
@@ -9,8 +9,13 @@ PASSWORD_FILE = tempfile.NamedTemporaryFile()
 PASSWORD_FILE.write(PASSWORD.encode('utf-8'))
 PASSWORD_FILE.flush()
 
+DUMMY_DATA_FILE = tempfile.NamedTemporaryFile()
+DUMMY_DATA_FILE.write('some data to back up'.encode('utf-8'))
+DUMMY_DATA_FILE.flush()
+
 restic.binary_path = 'restic'
 restic.repository = tempfile.mkdtemp()
 restic.password_file = PASSWORD_FILE.name
 
 restic.init()
+restic.backup([DUMMY_DATA_FILE.name])
