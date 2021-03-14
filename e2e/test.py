b9b2b094b45eb95c9e36988f4e3d66e8f783ed95
Michael Lynch <git@mtlynch.io>
2021-03-14 16:00:20 -0400

    Switch e2e tests to use logging module

--- e2e/test.py
+++ e2e/test.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
 
+import logging
 import os.path
 import tempfile
 
 import restic
+
+logger = logging.getLogger(__name__)
+
+
+def configure_logging():
+
+    class ShutdownHandler(logging.StreamHandler):
+
+        def emit(self, record):
+            super().emit(record)
+            if record.levelno >= logging.CRITICAL:
+                raise SystemExit(255)
+
+    root_logger = logging.getLogger()
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(name)-15s %(levelname)-4s %(message)s')
+    handler.setFormatter(formatter)
+    root_logger.addHandler(handler)
+    root_logger.addHandler(ShutdownHandler())
+    root_logger.setLevel(logging.DEBUG)
+
+
+configure_logging()
 
 PASSWORD = 'mysecretpass'
 PASSWORD_FILE = tempfile.NamedTemporaryFile()
@@ -19,24 +43,22 @@ restic.binary_path = 'restic'
 restic.repository = tempfile.mkdtemp()
 restic.password_file = PASSWORD_FILE.name
 
-print('Initializing repository')
+logger.info('Initializing repository')
 restic.init()
 
-print('Backing up %s' % DUMMY_DATA_PATH)
+logger.info('Backing up %s', DUMMY_DATA_PATH)
 restic.backup(paths=[DUMMY_DATA_PATH])
 
 RESTORE_DIR = tempfile.mkdtemp()
-print('Restoring to %s' % RESTORE_DIR)
+logger.info('Restoring to %s', RESTORE_DIR)
 restic.restore(snapshot_id='latest', target_dir=RESTORE_DIR)
 
 RESTORED_DATA_PATH = os.path.join(RESTORE_DIR, DUMMY_DATA_PATH)
 if not os.path.exists(RESTORED_DATA_PATH):
-    print('Expected to find %s' % RESTORED_DATA_PATH)
-    exit(1)
+    logger.fatal('Expected to find %s', RESTORED_DATA_PATH)
 RESTORED_DATA = open(RESTORED_DATA_PATH).read()
 if RESTORED_DATA != 'some data to back up':
-    print('Expected to restored file to contain %s (got %s)' %
-          ('some data to back up', RESTORED_DATA))
-    exit(1)
+    logger.fatal('Expected to restored file to contain %s (got %s)',
+                 'some data to back up', RESTORED_DATA)
 
-print('End-to-end test succeeded!')
+logger.info('End-to-end test succeeded!')
