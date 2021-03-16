838e2711087b43ab4c2aad90466e3da870057233
Michael Lynch <git@mtlynch.io>
2021-03-15 20:18:04 -0400

    Fix unit tests

--- restic/internal/forget_test.py
+++ restic/internal/forget_test.py
@@ -12,23 +12,27 @@ class ForgetTest(unittest.TestCase):
 
     @mock.patch.object(forget.command_executor, 'execute')
     def test_forget(self, mock_execute):
+        mock_execute.return_value = '{}'
         restic.forget()
         mock_execute.assert_called_with(['restic', '--json', 'forget'])
 
     @mock.patch.object(forget.command_executor, 'execute')
     def test_forget_and_prune(self, mock_execute):
+        mock_execute.return_value = '{}'
         restic.forget(prune=True)
         mock_execute.assert_called_with(
             ['restic', '--json', 'forget', '--prune'])
 
     @mock.patch.object(forget.command_executor, 'execute')
     def test_forget_all_except_last_30_days(self, mock_execute):
+        mock_execute.return_value = '{}'
         restic.forget(keep_daily=30)
         mock_execute.assert_called_with(
             ['restic', '--json', 'forget', '--keep-daily', '30'])
 
     @mock.patch.object(forget.command_executor, 'execute')
     def test_forget_all_except_last_30_days_and_prune(self, mock_execute):
+        mock_execute.return_value = '{}'
         restic.forget(prune=True, keep_daily=30)
         mock_execute.assert_called_with(
             ['restic', '--json', 'forget', '--prune', '--keep-daily', '30'])
