0b3b744a1fb00b5b08a647e38a10cc6a30bd0cec
Michael Lynch <git@mtlynch.io>
2021-03-21 21:46:06 -0400

    Add support for stats --mode flag

--- restic/internal/stats_test.py
+++ restic/internal/stats_test.py
@@ -17,5 +17,19 @@ class SelfUpdateTest(unittest.TestCase):
             "total_file_count": 3
             }
             """
-        restic.stats()
+
+        self.assertEqual({
+            'total_size': 20,
+            'total_file_count': 3
+        }, restic.stats())
+
         mock_execute.assert_called_with(['restic', '--json', 'stats'])
+
+    @mock.patch.object(stats.command_executor, 'execute')
+    def test_stats_with_mode(self, mock_execute):
+        mock_execute.return_value = '{}'
+
+        restic.stats(mode='blobs-per-file')
+
+        mock_execute.assert_called_with(
+            ['restic', '--json', 'stats', '--mode', 'blobs-per-file'])
