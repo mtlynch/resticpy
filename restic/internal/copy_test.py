fe676b0b9738337a6d620082853baf160ea816a0
Michael Lynch <git@mtlynch.io>
2021-03-28 21:00:21 -0400

    Add more realistic return value

--- restic/internal/copy_test.py
+++ restic/internal/copy_test.py
@@ -12,7 +12,11 @@ class CopyTest(unittest.TestCase):
 
     @mock.patch.object(copy.command_executor, 'execute')
     def test_copy_simple(self, mock_execute):
-        mock_execute.return_value = ''
+        mock_execute.return_value = """
+snapshot 670792c6 of [/tmp/tmp9k613t9a/mydata.txt] at 2021-03-29 00:56:31.183738563 +0000 UTC)
+  copy started, this may take a while...
+snapshot 3671204c saved
+""".strip()
 
         restic.copy(repo2='s3:https://dummyrepo.example.com/bucket',
                     password_file2='/dummy/pass.txt')
