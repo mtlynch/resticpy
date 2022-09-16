3809201375d6e999bfefc636f8ee88137df9a6b8
Matthias Baur <baurmatt@users.noreply.github.com>
2022-09-16 16:41:35 +0200

    Implement more flags for 'restic forget' (#86)
    * Implement keep-(last|hourly|daily|weekly|monthly|yearly) for 'restic forget'
    
    * Implement dry-run for 'restic forget'
    
    * Sort forget parameters alphabetically

--- restic/internal/forget.py
+++ restic/internal/forget.py
@@ -5,23 +5,47 @@ from restic.internal import command_executor
 
 
 def run(restic_base_command,
-        prune=False,
+        dry_run=None,
+        group_by=None,
+        keep_last=None,
+        keep_hourly=None,
         keep_daily=None,
+        keep_weekly=None,
+        keep_monthly=None,
+        keep_yearly=None,
         keep_within=None,
-        group_by=None):
+        prune=False):
     cmd = restic_base_command + ['forget']
+
+    if dry_run:
+        cmd.extend(['--dry-run'])
+
+    if group_by:
+        cmd.extend(['--group-by', group_by])
+
+    if keep_last:
+        cmd.extend(['--keep-last', str(keep_last)])
+
+    if keep_hourly:
+        cmd.extend(['--keep-hourly', str(keep_hourly)])
+
+    if keep_daily:
+        cmd.extend(['--keep-daily', str(keep_daily)])
+
+    if keep_weekly:
+        cmd.extend(['--keep-weekly', str(keep_weekly)])
+
+    if keep_monthly:
+        cmd.extend(['--keep-monthly', str(keep_monthly)])
+
+    if keep_yearly:
+        cmd.extend(['--keep-yearly', str(keep_yearly)])
+
+    if keep_within:
+        cmd.extend(['--keep-within', str(keep_within)])
 
     if prune:
         cmd.extend(['--prune'])
-
-    if keep_daily is not None:
-        cmd.extend(['--keep-daily', str(keep_daily)])
-
-    if keep_within is not None:
-        cmd.extend(['--keep-within', keep_within])
-
-    if group_by is not None:
-        cmd.extend(['--group-by', group_by])
 
     return _parse_result(command_executor.execute(cmd))
 
--- restic/internal/forget_test.py
+++ restic/internal/forget_test.py
@@ -20,11 +20,34 @@ class ForgetTest(unittest.TestCase):
         mock_execute.assert_called_with(['restic', '--json', 'forget'])
 
     @mock.patch.object(forget.command_executor, 'execute')
-    def test_forget_and_prune(self, mock_execute):
+    def test_dry_run(self, mock_execute):
         mock_execute.return_value = '{}'
-        restic.forget(prune=True)
+
+        restic.forget(dry_run=True)
+
         mock_execute.assert_called_with(
-            ['restic', '--json', 'forget', '--prune'])
+            ['restic', '--json', 'forget', '--dry-run'])
+
+    @mock.patch.object(forget.command_executor, 'execute')
+    def test_forget_with_group_by(self, mock_execute):
+        mock_execute.return_value = '{}'
+        restic.forget(prune=True, group_by='host')
+        mock_execute.assert_called_with(
+            ['restic', '--json', 'forget', '--group-by', 'host', '--prune'])
+
+    @mock.patch.object(forget.command_executor, 'execute')
+    def test_forget_keep_last_10(self, mock_execute):
+        mock_execute.return_value = '{}'
+        restic.forget(keep_last=10)
+        mock_execute.assert_called_with(
+            ['restic', '--json', 'forget', '--keep-last', '10'])
+
+    @mock.patch.object(forget.command_executor, 'execute')
+    def test_forget_keep_hourly_10(self, mock_execute):
+        mock_execute.return_value = '{}'
+        restic.forget(keep_hourly=10)
+        mock_execute.assert_called_with(
+            ['restic', '--json', 'forget', '--keep-hourly', '10'])
 
     @mock.patch.object(forget.command_executor, 'execute')
     def test_forget_all_except_last_30_days(self, mock_execute):
@@ -34,25 +57,46 @@ class ForgetTest(unittest.TestCase):
             ['restic', '--json', 'forget', '--keep-daily', '30'])
 
     @mock.patch.object(forget.command_executor, 'execute')
+    def test_forget_keep_weekly_10(self, mock_execute):
+        mock_execute.return_value = '{}'
+        restic.forget(keep_weekly=10)
+        mock_execute.assert_called_with(
+            ['restic', '--json', 'forget', '--keep-weekly', '10'])
+
+    @mock.patch.object(forget.command_executor, 'execute')
+    def test_forget_keep_monthly_10(self, mock_execute):
+        mock_execute.return_value = '{}'
+        restic.forget(keep_monthly=10)
+        mock_execute.assert_called_with(
+            ['restic', '--json', 'forget', '--keep-monthly', '10'])
+
+    @mock.patch.object(forget.command_executor, 'execute')
+    def test_forget_keep_yearly_10(self, mock_execute):
+        mock_execute.return_value = '{}'
+        restic.forget(keep_yearly=10)
+        mock_execute.assert_called_with(
+            ['restic', '--json', 'forget', '--keep-yearly', '10'])
+
+    @mock.patch.object(forget.command_executor, 'execute')
     def test_forget_all_except_last_30_days_and_prune(self, mock_execute):
         mock_execute.return_value = '{}'
         restic.forget(prune=True, keep_daily=30)
         mock_execute.assert_called_with(
-            ['restic', '--json', 'forget', '--prune', '--keep-daily', '30'])
+            ['restic', '--json', 'forget', '--keep-daily', '30', '--prune'])
 
     @mock.patch.object(forget.command_executor, 'execute')
-    def test_forget_with_group_by(self, mock_execute):
+    def test_forget_and_prune(self, mock_execute):
         mock_execute.return_value = '{}'
-        restic.forget(prune=True, group_by='host')
+        restic.forget(prune=True)
         mock_execute.assert_called_with(
-            ['restic', '--json', 'forget', '--prune', '--group-by', 'host'])
+            ['restic', '--json', 'forget', '--prune'])
 
     @mock.patch.object(forget.command_executor, 'execute')
     def test_forget_and_keep_within(self, mock_execute):
         mock_execute.return_value = '{}'
         restic.forget(prune=True, keep_within='60d')
         mock_execute.assert_called_with(
-            ['restic', '--json', 'forget', '--prune', '--keep-within', '60d'])
+            ['restic', '--json', 'forget', '--keep-within', '60d', '--prune'])
 
     @mock.patch.object(forget.command_executor, 'execute')
     def test_parses_result_json(self, mock_execute):
