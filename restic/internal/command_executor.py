07714cf450f94ac7d306119eaef63f20c8e7c559
Matthias Baur <m.baur@syseleven.de>
2022-12-12 15:05:42 +0100

    restic cat pack returns binary data, support it

--- restic/internal/command_executor.py
+++ restic/internal/command_executor.py
@@ -6,14 +6,20 @@ import restic.errors
 logger = logging.getLogger(__name__)
 
 
-def execute(cmd):
+def execute(cmd, binary_mode=False):
     logger.debug('Executing restic command: %s', str(cmd))
     try:
-        process = subprocess.run(cmd,
-                                 capture_output=True,
-                                 text=True,
-                                 check=False,
-                                 encoding='utf-8')
+        subprocess_args = {
+            'capture_output': True,
+            'check': False,
+            'text': True,
+            'encoding': 'utf-8'
+        }
+        if binary_mode:
+            subprocess_args.pop('text')
+            subprocess_args.pop('encoding')
+
+        process = subprocess.run(cmd, **subprocess_args)
     except FileNotFoundError as e:
         raise restic.errors.NoResticBinaryEror(
             'Cannot find restic installed') from e
@@ -24,6 +30,6 @@ def execute(cmd):
     if process.returncode != 0:
         raise restic.errors.ResticFailedError(
             f'Restic failed with exit code {process.returncode}: ' +
-            process.stderr)
+            str(process.stderr))
 
     return process.stdout
