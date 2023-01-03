fc8b3e29d2a7a55f9ddfdb8150a6935fda3bd8ca
worldworm <13227454+worldworm@users.noreply.github.com>
2023-01-03 17:44:36 +0000

    fixes linter errors

--- restic/internal/init.py
+++ restic/internal/init.py
@@ -3,14 +3,18 @@ import re
 from restic.internal import command_executor
 
 
-def run(restic_base_command, copy_chunker_params=False, from_repo=None, from_password_file=None, repo2=None): # FIXME: remove repo2 in next major version
+def run(restic_base_command,
+        copy_chunker_params=False,
+        from_repo=None,
+        from_password_file=None,
+        repo2=None):  # FIXME: remove repo2 in next major version
     cmd = restic_base_command + ['init']
 
     if copy_chunker_params:
         cmd.append('--copy-chunker-params')
 
-    if repo2: # FIXME: remove in next major version
-        print("Arg repo2 has been deprecated, use from-repo instead")
+    if repo2:  # FIXME: remove in next major version
+        print('Arg repo2 has been deprecated, use from-repo instead')
         cmd.extend(['--repo2', repo2])
 
     if from_repo:
