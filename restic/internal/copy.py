121a56e4736bdb431f9a4e6023e6926d0589203d
worldworm <13227454+worldworm@users.noreply.github.com>
2023-01-04 12:36:30 +0000

    removes deprecated repo2

--- restic/internal/copy.py
+++ restic/internal/copy.py
@@ -1,24 +1,9 @@
 from restic.internal import command_executor
 
 
-def run(restic_base_command,
-        repo2=None,
-        password_file2=None,
-        from_repo=None,
-        from_password_file=None
-       ):  # FIXME: remove repo2, password_file2 in next major version
+def run(restic_base_command, from_repo=None, from_password_file=None):
 
     cmd = restic_base_command + ['copy']
-
-    if repo2:  # FIXME: remove in next major version
-        print('Arg repo2 has been deprecated, use from-repo instead')
-        cmd.extend(['--repo2', repo2])
-
-    if password_file2:  # FIXME: remove in next major version
-        print(
-            'Arg password_file2 has been deprecated, use from-password-file instead'  # pylint: disable=C0301
-        )
-        cmd.extend(['--password-file2', password_file2])
 
     if from_repo:
         cmd.extend(['--from-repo', from_repo])
