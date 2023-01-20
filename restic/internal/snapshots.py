ac3f543a68781e5e9080d73ca1758301c9c29ee5
Matthias Baur <m.baur@syseleven.de>
2023-01-20 07:54:18 +0100

    Use append() instead of extend()

--- restic/internal/snapshots.py
+++ restic/internal/snapshots.py
@@ -7,7 +7,7 @@ def run(restic_base_command, snapshot_id=None, group_by=None):
     cmd = restic_base_command + ['snapshots']
 
     if snapshot_id:
-        cmd.extend([snapshot_id])
+        cmd.append(snapshot_id)
 
     if group_by:
         cmd.extend(['--group-by', group_by])
