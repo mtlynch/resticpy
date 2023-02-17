c62c5341175eb04809daa6f9f7e3176896c86f0e
Matthias Baur <baurmatt@users.noreply.github.com>
2023-02-17 02:11:33 +0100

    Support 'restic check --read-data-subset' (#130)

--- restic/internal/check.py
+++ restic/internal/check.py
@@ -2,11 +2,14 @@ import restic.errors
 from restic.internal import command_executor
 
 
-def run(restic_base_command, read_data=False):
+def run(restic_base_command, read_data=False, read_data_subset=None):
     cmd = restic_base_command + ['check']
 
     if read_data:
         cmd.append('--read-data')
+
+    if read_data_subset:
+        cmd.extend(['--read-data-subset', read_data_subset])
 
     try:
         return command_executor.execute(cmd)
