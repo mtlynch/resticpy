2c0f2f8f17d8c3a1bc0eab232ab088b1ccf4b017
Michael Lynch <mtlynch@users.noreply.github.com>
2023-12-02 12:16:41 -0800

    Simplify tags in forget test (#142)
    It's a little unexpected for there to be a tag with a comma within it,
    so I'm simplifying to two standard-looking tags.

--- restic/internal/forget_test.py
+++ restic/internal/forget_test.py
@@ -48,10 +48,10 @@ class ForgetTest(unittest.TestCase):
     @mock.patch.object(forget.command_executor, 'execute')
     def test_forget_with_multiple_tags(self, mock_execute):
         mock_execute.return_value = '{}'
-        restic.forget(prune=True, tags=['musician1,musician2', 'musician3'])
+        restic.forget(prune=True, tags=['musician1', 'musician2'])
         mock_execute.assert_called_with([
-            'restic', '--json', 'forget', '--tag', 'musician1,musician2',
-            '--tag', 'musician3', '--prune'
+            'restic', '--json', 'forget', '--tag', 'musician1', '--tag',
+            'musician2', '--prune'
         ])
 
     @mock.patch.object(forget.command_executor, 'execute')
