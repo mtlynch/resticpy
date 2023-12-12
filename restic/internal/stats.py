c6ee4173318187b9392e65cd44cfa116708153bf
Michael Schulze <30439703+mschulze46@users.noreply.github.com>
2023-12-12 02:28:01 +0100

    stats: Added support for tags and host options (#145)
    Changes:
    - `restic/internal/stats.py`: Added tags and host options
    - `restic/internal/stats_test.py`: Added tests for new options
    - `docs/index.md`: Added new options to documentation
    
    Many thanks for your efforts!
    
    Co-authored-by: michael.schulze <michael.schulze@sps-itsec.de>

--- restic/internal/stats.py
+++ restic/internal/stats.py
@@ -3,10 +3,17 @@ import json
 from restic.internal import command_executor
 
 
-def run(restic_base_command, mode=None):
+def run(restic_base_command, mode=None, tags=None, host=None):
     cmd = restic_base_command + ['stats']
 
     if mode:
         cmd.extend(['--mode', mode])
+
+    if tags:
+        for tag in tags:
+            cmd.extend(['--tag', tag])
+
+    if host:
+        cmd.extend(['--host', host])
 
     return json.loads(command_executor.execute(cmd))
