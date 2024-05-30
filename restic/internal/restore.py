e6a5e57655d2ae7c3633b99887765f9a6ed2d19d
Thibault Soubiran <soubi.thibault@gmail.com>
2024-05-31 00:04:58 +0200

    Support exclude flag for restore command (#160)
    Add the `--exclude` parameter to the restore command.
    
    Resolves: #159

--- restic/internal/restore.py
+++ restic/internal/restore.py
@@ -4,11 +4,15 @@ from restic.internal import command_executor
 def run(restic_base_command,
         snapshot_id='latest',
         include=None,
+        exclude=None,
         target_dir=None):
     cmd = restic_base_command + ['restore', snapshot_id]
 
     if include:
         cmd.extend(['--include', include])
+
+    if exclude:
+        cmd.extend(['--exclude', exclude])
 
     if target_dir:
         cmd.extend(['--target', target_dir])
