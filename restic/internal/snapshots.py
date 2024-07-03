6cad37f55e805e98ad0b1c746a2f799bfa0212ce
Jeroen Wellner <jeroen@wllnr.nl>
2024-07-03 16:19:20 +0200

    Added 'path' and 'host' arguments to snapshots command

--- restic/internal/snapshots.py
+++ restic/internal/snapshots.py
@@ -3,7 +3,7 @@ import json
 from restic.internal import command_executor
 
 
-def run(restic_base_command, snapshot_id=None, group_by=None, tags=None):
+def run(restic_base_command, snapshot_id=None, group_by=None, tags=None, path=None, host=None):
     cmd = restic_base_command + ['snapshots']
 
     if snapshot_id:
@@ -14,5 +14,11 @@ def run(restic_base_command, snapshot_id=None, group_by=None, tags=None):
 
     if tags:
         cmd.extend(['--tag', ','.join(tags)])
+
+    if path:
+        cmd.extend(['--path', path])
+
+    if host:
+        cmd.extend(['--host', host])
 
     return json.loads(command_executor.execute(cmd))
