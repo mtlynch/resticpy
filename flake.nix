2a47224897b419509ac3323810d1739b81f068e0
Michael Lynch <git@mtlynch.io>
2024-09-26 20:17:42 -0400

    Refactor / reformat Nix flake

--- flake.nix
+++ flake.nix
@@ -1,47 +1,44 @@
 {
   description = "Create Nix development environment";
 
-  # Python 3.12.1 release
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/fd04bea4cbf76f86f244b9e2549fca066db8ddff";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+    # 3.12.1 release
+    python-nixpkgs.url = "github:NixOS/nixpkgs/fd04bea4cbf76f86f244b9e2549fca066db8ddff";
 
-  inputs.pyproject-nix.url = "github:nix-community/pyproject.nix";
-  # Don't use the pyproject.nix flake directly to avoid its inputs in our
-  # closure.
-  inputs.pyproject-nix.flake = false;
+    pyproject-nix = {
+      url = "github:nix-community/pyproject.nix";
+      flake = false;
+    };
+  };
 
-  outputs = { self, nixpkgs, flake-utils, pyproject-nix  }:
-    let
-      pyproject = import (pyproject-nix + "/lib") { inherit (nixpkgs) lib; };
-
-      # Load/parse dev_requirements.txt
+  outputs = {
+    self,
+    flake-utils,
+    python-nixpkgs,
+    pyproject-nix,
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = python-nixpkgs.legacyPackages.${system};
+      python = pkgs.python312;
+      pyproject = import (pyproject-nix + "/lib") {inherit (pkgs) lib;};
       project = pyproject.project.loadRequirementsTxt {
         requirements = ./dev_requirements.txt;
       };
+      pythonEnv = pkgs.python3.withPackages (pyproject.renderers.withPackages {
+        inherit project python;
+      });
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pythonEnv
+        ];
+        shellHook = ''
+          python --version
+        '';
+      };
 
-    in
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      python = pkgs.python312;
-
-      pythonEnv = (
-          # Render requirements.txt into a Python withPackages environment.
-          pkgs.python3.withPackages (pyproject.renderers.withPackages {
-            inherit project python;
-          })
-        );
-    in
-    {
-      devShell =
-        pkgs.mkShell {
-          packages = [
-            pythonEnv
-          ];
-          shellHook = ''
-            python --version
-          '';
-        };
+      formatter = pkgs.alejandra;
     });
 }
