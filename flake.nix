02dac3b049f449589b264ffb553c699eebfae576
Michael Lynch <mtlynch@users.noreply.github.com>
2025-10-25 20:10:45 -0400

    Add restic to Nix flake (#198)

--- flake.nix
+++ flake.nix
@@ -10,6 +10,9 @@
     # 20.6.1 release
     nodejs-nixpkgs.url = "github:NixOS/nixpkgs/78058d810644f5ed276804ce7ea9e82d92bee293";
 
+    # 0.17.1 release
+    restic-nixpkgs.url = "github:NixOS/nixpkgs/d4f247e89f6e10120f911e2e2d2254a050d0f732";
+
     pyproject-nix = {
       url = "github:nix-community/pyproject.nix";
       flake = false;
@@ -21,12 +24,14 @@
     flake-utils,
     python-nixpkgs,
     nodejs-nixpkgs,
+    restic-nixpkgs,
     pyproject-nix,
   } @ inputs:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = python-nixpkgs.legacyPackages.${system};
       python = pkgs.python312;
       nodejs = nodejs-nixpkgs.legacyPackages.${system}.nodejs_20;
+      restic = restic-nixpkgs.legacyPackages.${system}.restic;
       pyproject = import (pyproject-nix + "/lib") {inherit (pkgs) lib;};
       project = pyproject.project.loadRequirementsTxt {
         requirements = ./dev_requirements.txt;
@@ -39,10 +44,12 @@
         packages = [
           pythonEnv
           nodejs
+          restic
         ];
         shellHook = ''
           echo "node" "$(node --version)"
           echo "npm" "$(npm --version)"
+          restic version
           python --version
         '';
       };
