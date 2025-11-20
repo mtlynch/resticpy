c3484403dd244e0475d10f431049642bbabb8531
Michael Lynch <git@mtlynch.io>
2025-11-20 15:46:50 -0500

    Change pylint warnings to use symbolic names

--- restic/internal/find_test.py
+++ restic/internal/find_test.py
@@ -22,8 +22,7 @@ class FindTest(unittest.TestCase):
 [{"matches":[{"path":"/dummyfile","permissions":"-rw-r--r--","type":"file","mode":420,"mtime":"2022-01-01T10:00:00.000000000-07:00","atime":"2022-01-01T10:00:00.000000000-07:00","ctime":"2022-01-01T10:00:00.000000000-07:00","uid":10000,"gid":10000,"user":"user","group":"group","device_id":10000,"size":1024,"links":1}],"hits":1,"snapshot":"f589421bafdae95f5be5eea6285074b7ddc54aa0ffd1ad606f74d1e6207d20a3"}]
 '''.strip()
 
-        # Ignore complaint about too long of a line.
-        # pylint: disable=C0301
+        # pylint: disable-next=line-too-long
         self.assertEqual([{
             'matches': [{
                 'path': '/dummyfile',
@@ -54,8 +53,7 @@ class FindTest(unittest.TestCase):
 [{"matches":[{"path":"/dummyfile1","permissions":"-rw-r--r--","type":"file","mode":420,"mtime":"2022-01-01T10:00:00.000000000-07:00","atime":"2022-01-01T10:00:00.000000000-07:00","ctime":"2022-01-01T10:00:00.000000000-07:00","uid":10000,"gid":10000,"user":"user","group":"group","device_id":10000,"size":1024,"links":1},{"path":"/dummyfile2","permissions":"-rw-r--r--","type":"file","mode":420,"mtime":"2022-01-01T10:00:00.000000000-07:00","atime":"2022-01-01T10:00:00.000000000-07:00","ctime":"2022-01-01T10:00:00.000000000-07:00","uid":10000,"gid":10000,"user":"user","group":"group","device_id":10000,"size":1024,"links":1}],"hits":2,"snapshot":"f589421bafdae95f5be5eea6285074b7ddc54aa0ffd1ad606f74d1e6207d20a3"}]
 '''.strip()
 
-        # Ignore complaint about too long of a line.
-        # pylint: disable=C0301
+        # pylint: disable-next=line-too-long
         self.assertEqual([{
             'matches': [{
                 'path': '/dummyfile1',
@@ -101,8 +99,7 @@ class FindTest(unittest.TestCase):
 [{"matches":[{"path":"/dummyfile1","permissions":"-rw-r--r--","type":"file","mode":420,"mtime":"2022-01-01T10:00:00.000000000-07:00","atime":"2022-01-01T10:00:00.000000000-07:00","ctime":"2022-01-01T10:00:00.000000000-07:00","uid":10000,"gid":10000,"user":"user","group":"group","device_id":10000,"size":1024,"links":1}],"hits":1,"snapshot":"f589421bafdae95f5be5eea6285074b7ddc54aa0ffd1ad606f74d1e6207d20a3"},{"matches":[{"path":"/dummyfile2","permissions":"-rw-r--r--","type":"file","mode":420,"mtime":"2022-01-01T10:00:00.000000000-07:00","atime":"2022-01-01T10:00:00.000000000-07:00","ctime":"2022-01-01T10:00:00.000000000-07:00","uid":10000,"gid":10000,"user":"user","group":"group","device_id":10000,"size":1024,"links":1}],"hits":1,"snapshot":"g589421bafdae95f5be5eea6285074b7ddc54aa0ffd1ad606f74d1e6207d20a3"}]
 '''.strip()
 
-        # Ignore complaint about too long of a line.
-        # pylint: disable=C0301
+        # pylint: disable-next=line-too-long
         self.assertEqual([{
             'matches': [{
                 'path': '/dummyfile1',
